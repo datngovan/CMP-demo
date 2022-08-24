6cba2c2211b4476bd953c32a8ec100f01516e4ef
datngovan <s3817813@rmit.edu.vn>
2022-08-24 16:31:49 +0700

    add CMP

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import CommandPalette from 'command-palette-vue3';
+import "command-palette-vue3/dist/style.css";
+import {ref, computed} from "vue";
+// Optional can disable display by group by adding return false to the isDisplayByGroup
+  const isDisplayByGroup = computed(() => {
+  return true;
+  });
+  // Optional delete if donot want to using default light and dark theme
+  const themeMode = ref({
+    dark: true,
+    light: false
+  });
+  /*
+  Optional delete if donot want to using changeTheme function can assign theme directly
+  by adding class="light" or class="dark" to <CommandPalette>
+  */
+  const changeTheme=()=>{
+    themeMode.value.dark=!themeMode.value.dark;
+    themeMode.value.light=!themeMode.value.light;
+  };
+  const customerGroups = computed(() => {
+  return [
+    {groupName:"Default",
+    commands:[
+      {commandName:"Switch Light/Dark Theme", 
+      commandKey:"Control+m", 
+      commandAction:() => {changeTheme();}},
+      {commandName:"Test Command", 
+      commandKey:"Control+Shift+m", 
+      commandAction:() => {alert("Test Command excecute.")}},
+      ]},
+  ];
+});
 </script>
 
 <template>
   <div>
+    <CommandPalette :themeMode="themeMode" :customerGroups="customerGroups" :isDisplayByGroup="isDisplayByGroup"/>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
