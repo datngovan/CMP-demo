61ead9b6cc6adebeb3746de12b20e41556ec6df1
datngovan <s3817813@rmit.edu.vn>
2022-08-30 15:45:19 +0700

    improve UI

--- src/layout/demo.vue
+++ src/layout/demo.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
 import CommandPalette from "command-palette-vue3";
 import "command-palette-vue3/dist/style.css";
 import { ref, computed, onMounted } from "vue";
@@ -48,7 +47,9 @@ const customerGroups = computed(() => {
           commandKey: "Control+m",
           commandAction: () => {
             changeTheme();
-            var element = document.getElementById('theme-checkbox') as HTMLInputElement;
+            var element = document.getElementById(
+              "theme-checkbox"
+            ) as HTMLInputElement;
             element.checked = !element.checked;
           },
         },
@@ -81,121 +82,173 @@ onMounted(() => {
 </script>
 
 <template>
-    <header class="header">
-    <a href="/">
-        <img alt="logo"/>
-    </a>
-    <p style="padding-left: 800px;">
-      <router-link class="link" to="/demo">Demo</router-link>
-      <a href="https://www.npmjs.com/package/command-palette-vue3" target="_blank" rel="noopener noreferrer" class="link">Documentation</a>
-    </p>
+  <div>
     <div>
-      <ul class="list" style="gap:0px !important;">
-        <li>
-          <a href="https://github.com/nntai/vue-command-palette" class="icon"><svg  xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="#42b883" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
-        </li>
-        <li>
-          <a href="https://github.com/nntai/vue-command-palette" class="icon"><svg  xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="#42b883" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>  
-        </li>
-      </ul>
-    </div>
-  </header>
-  <main>
-    <div v-bind:class="themeMode.light ? 'lightMode' : ''">
-      <h1 style="margin-bottom: 200px">
-        Activate the Command Palette using Ctrl + K
-      </h1>
-      <CommandPalette
-        class="cmp"
-        :themeMode="themeMode"
-        :customerGroups="customerGroups"
-        :isDisplayByGroup="isDisplayByGroup"
-      />
-    </div>
-    <section class="demo-section">
-      <div class="demo-text">
-        <h3>Control application state</h3>
-        <p class="p-text">You can increase the <b>Count</b> by <b>One</b><br /></p>
-        <p class="p-text">Using the button on the bottom right.<br /></p>
-        <p class="p-text">
-          We also have the the Command Palette function and keyboard shortcut.
-        </p>
-      </div>
-      <div class="demo-result">
-        <div class="count">{{ count }}</div>
-        <div>
-          <button @click="count++">Plus One</button>
-          <button @click="count--">Minus One</button>
+      <header class="header">
+        <div style="text-align: left">
+          <a href="/">
+            <img alt="logo" />
+          </a>
         </div>
-        <p class="descr p-text">
-          Try Press <kbd class="keyboard">Ctrl</kbd>
-          <kbd class="keyboard">N</kbd> / <kbd class="keyboard">Ctrl</kbd>
-          <kbd class="keyboard">V</kbd>
-        </p>
+        <div style="display: flex; text-align: right; margin-left: auto">
+          <p>
+            <router-link class="link" to="/demo">Demo</router-link>
+            <a
+              href="https://www.npmjs.com/package/command-palette-vue3"
+              id="documentation-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="link"
+              >Documentation</a
+            >
+          </p>
+          <ul class="list">
+            <li>
+              <a
+                href="https://github.com/nntai/vue-command-palette"
+                class="icon"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#42b883"
+                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                  /></svg></a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/nntai/vue-command-palette"
+                class="icon"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#42b883"
+                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                  /></svg></a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+    <main>
+      <div v-bind:class="themeMode.light ? 'lightMode' : ''">
+        <h1 style="margin-bottom: 200px">
+          Activate the Command Palette using Ctrl + K
+        </h1>
+        <CommandPalette
+          class="cmp"
+          :themeMode="themeMode"
+          :customerGroups="customerGroups"
+          :isDisplayByGroup="isDisplayByGroup"
+        />
       </div>
-    </section>
-    <section class="demo-section">
-      <div class="demo-text">
-        <h3>Enable Conditional actions</h3>
-        <p class="p-text">You toggle the 1st <b>button</b> to <b>Switch Theme</b>,<br /></p>
-        <p class="p-text">
-          and the 2nd <b>button</b> to enable/disable <b>Group Search</b><br />
-        </p>
-        <p class="p-text">
-          We also have the the Command Palette function and keyboard shortcut.
-        </p>
-      </div>
-      <div class="demo-result">
-        <label for="group-search">
-          <input
-            type="checkbox"
-            name="group-search"
-            class="hidden-box"
-            @click="
-              {
-                changeTheme();
-              }
-            "
-          />
-          <strong>Result Display in Groups</strong>
-        </label>
-        <p class="descr p-text">
-          Try Press <kbd class="keyboard">Ctrl</kbd>
-          <kbd class="keyboard">D</kbd>
-        </p>
-      </div>
-    </section>
-    <section class="demo-section">
-      <div class="demo-text">
-        <h3>Change Theme</h3>
-        <p class="p-text">You toggle the 1st <b>button</b> to <b>Switch Theme</b>.<br /></p>
-        <p class="p-text">
-          We also have the the Command Palette function and keyboard shortcut.
-        </p>
-      </div>
-      <div class="demo-result">
-        <div>Current Theme is: </div>
-        <div class="count" v-if="themeMode.dark===true">Dark</div>
-        <div class="count" v-else>Light</div>
-        <div>
-          <label class="switch">
-            <input type="checkbox" id="theme-checkbox" v-on:click="{changeTheme();}"/>
-            <span class="slider round"></span>
+      <section class="demo-section">
+        <div class="demo-text">
+          <h3>Control application state</h3>
+          <p class="p-text">
+            You can increase the <b>Count</b> by <b>One</b><br />
+          </p>
+          <p class="p-text">Using the button on the bottom right.<br /></p>
+          <p class="p-text">
+            We also have the the Command Palette function and keyboard shortcut.
+          </p>
+        </div>
+        <div class="demo-result">
+          <div class="count">{{ count }}</div>
+          <div>
+            <button @click="count++">Plus One</button>
+            <button @click="count--">Minus One</button>
+          </div>
+          <p class="descr p-text">
+            Try Press <kbd class="keyboard">Ctrl</kbd>
+            <kbd class="keyboard">N</kbd> / <kbd class="keyboard">Ctrl</kbd>
+            <kbd class="keyboard">V</kbd>
+          </p>
+        </div>
+      </section>
+      <section class="demo-section">
+        <div class="demo-text">
+          <h3>Change Theme</h3>
+          <p class="p-text">
+            You toggle the 1st <b>button</b> to <b>Switch Theme</b>.<br />
+          </p>
+          <p class="p-text">
+            We also have the the Command Palette function and keyboard shortcut.
+          </p>
+        </div>
+        <div class="demo-result">
+          <div>Current Theme is:</div>
+          <div class="count" v-if="themeMode.dark === true">Dark</div>
+          <div class="count" v-else>Light</div>
+          <div>
+            <label class="switch">
+              <input
+                type="checkbox"
+                id="theme-checkbox"
+                v-on:click="
+                  {
+                    changeTheme();
+                  }
+                "
+              />
+              <span class="slider round"></span>
+            </label>
+          </div>
+          <p class="descr p-text">
+            Try Press <kbd class="keyboard">Ctrl</kbd>
+            <kbd class="keyboard">M</kbd>
+          </p>
+        </div>
+      </section>
+      <section class="demo-section">
+        <div class="demo-text">
+          <h3>Enable Conditional actions</h3>
+          <p class="p-text">
+            You toggle the 1st <b>button</b> to <b>Switch Theme</b>,<br />
+          </p>
+          <p class="p-text">
+            and the 2nd <b>button</b> to enable/disable <b>Group Search</b
+            ><br />
+          </p>
+          <p class="p-text">
+            We also have the the Command Palette function and keyboard shortcut.
+          </p>
+        </div>
+        <div class="demo-result">
+          <label for="group-search">
+            <input
+              type="checkbox"
+              name="group-search"
+              class="hidden-box"
+              @click="
+                {
+                  changeTheme();
+                }
+              "
+            />
+            <strong>Result Display in Groups</strong>
           </label>
+          <p class="descr p-text">
+            Try Press <kbd class="keyboard">Ctrl</kbd>
+            <kbd class="keyboard">D</kbd>
+          </p>
         </div>
-        <p class="descr p-text">
-          Try Press <kbd class="keyboard">Ctrl</kbd>
-          <kbd class="keyboard">M</kbd>
-        </p>
-      </div>
-    </section>
-  </main>
-  <footer>
-    Make By&nbsp;<span class="highlight">Team F4</span>&nbsp;and<span
-      class="highlight"
-      >Holistics</span
-    >
-  </footer>
+      </section>
+    </main>
+    <footer>
+      Make By&nbsp;<span class="highlight">Team F4</span>&nbsp;and<span
+        class="highlight"
+        >Holistics</span
+      >
+    </footer>
+  </div>
 </template>
 
 <style scoped>
--- src/layout/main.vue
+++ src/layout/main.vue
@@ -41,10 +41,9 @@ const minusOne = () => {
   return count.value--;
 };
 const saveTheme = () => {
-  if(themeMode.value.dark===true){
+  if (themeMode.value.dark === true) {
     window.sessionStorage.setItem("darkMode", "true");
-  }
-  else{
+  } else {
     window.sessionStorage.setItem("darkMode", "false");
   }
 };
@@ -79,8 +78,8 @@ const customerGroups = computed(() => {
   ];
 });
 
-onMounted(()=>{
-  if(window.sessionStorage.getItem("darkMode")==="false"){
+onMounted(() => {
+  if (window.sessionStorage.getItem("darkMode") === "false") {
     themeMode.value.dark = false;
     themeMode.value.light = true;
     document.getElementById("body")?.classList.add("lightMode");
@@ -89,107 +88,163 @@ onMounted(()=>{
 });
 </script>
 <template>
-  <div style="height:100vh;display: flex;flex-direction: column;">
-    <header class="header">
-    <a href="/">
-        <img alt="logo"/>
-    </a>
-    <p style="padding-left: 800px;">
-      <router-link class="link" to="/demo">Demo</router-link>
-      <a href="https://www.npmjs.com/package/command-palette-vue3" id="documentation-link" target="_blank" rel="noopener noreferrer" class="link">Documentation</a>
-    </p>
-    <div>
-      <ul class="list">
-        <li>
-          <a href="https://github.com/nntai/vue-command-palette" class="icon"><svg  xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="#42b883" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>
-        </li>
-        <li>
-          <a href="https://github.com/nntai/vue-command-palette" class="icon"><svg  xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="#42b883" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg></a>  
-        </li>
-      </ul>
-    </div>
-  </header>
-  <div v-bind:class="themeMode.light ? 'lightMode' : ''">
-    <CommandPalette
-      class="cmp"
-      :themeMode="themeMode"
-      :customerGroups="customerGroups"
-      :isDisplayByGroup="isDisplayByGroup"
-    />
-  </div>
-  <main>
-    <section class="main-section">
-      <main class="sologan" style="display:flex">
-        <h3><span>Make Your Web Application Faster Using </span> <br>
-       <span> Command Palette</span></h3>
-      </main>
-      <aside>
-        <div class="gwraper">
-          <figure>
-            <img src="https://solid-command-palette.vercel.app/images/command-palette-examples/tailwind.png" alt="demo1"/>
-          </figure>
-          <figcaption><span>Figure 1: Test 1</span></figcaption>
-          <figure>
-            <img src="https://solid-command-palette.vercel.app/images/command-palette-examples/tailwind.png" alt="demo1"/>
-          </figure>
-          <figcaption><span>Figure 1: Test 1</span></figcaption>
-          <figure>
-            <img src="https://solid-command-palette.vercel.app/images/command-palette-examples/tailwind.png" alt="demo1"/>
-          </figure>
-          <figcaption><span>Figure 1: Test 1</span></figcaption>
+  <div
+    style="
+      height: 100vh;
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+    "
+  >
+    <div style="display: inline-block">
+      <header class="header">
+        <div style="text-align: left">
+          <a href="/" style="display: inline-block;">
+            <img alt="logo" />
+          </a>
         </div>
-      </aside>
-  </section>
-  </main>
-  <footer>
-    Make By&nbsp;<span class="highlight">Team F4</span>&nbsp;and<span class="highlight">Holistics</span>
-  </footer>
+        <div style="display: flex; text-align: right; margin-left: auto;">
+          <p>
+            <router-link class="link" to="/demo">Demo</router-link>
+            <a
+              href="https://www.npmjs.com/package/command-palette-vue3"
+              id="documentation-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="link"
+              >Documentation</a
+            >
+          </p>
+          <ul class="list">
+            <li>
+              <a
+                href="https://github.com/nntai/vue-command-palette"
+                class="icon"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#42b883"
+                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                  /></svg></a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/nntai/vue-command-palette"
+                class="icon"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="40"
+                  height="40"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#42b883"
+                    d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+                  /></svg></a>
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+    <div v-bind:class="themeMode.light ? 'lightMode' : ''">
+      <CommandPalette
+        class="cmp"
+        :themeMode="themeMode"
+        :customerGroups="customerGroups"
+        :isDisplayByGroup="isDisplayByGroup"
+      />
+    </div>
+    <main>
+      <section class="main-section">
+        <main class="sologan" style="display: flex">
+          <h3>
+            <span>Make Your Web Application Faster Using </span> <br />
+            <span style="text-decoration: underline"> Command Palette</span>
+          </h3>
+        </main>
+        <aside>
+          <div class="gwraper">
+            <figure>
+              <img
+                src="https://solid-command-palette.vercel.app/images/command-palette-examples/tailwind.png"
+                alt="demo1"
+              />
+            </figure>
+            <figcaption><span>Figure 1: Test 1</span></figcaption>
+            <figure>
+              <img
+                src="https://solid-command-palette.vercel.app/images/command-palette-examples/tailwind.png"
+                alt="demo1"
+              />
+            </figure>
+            <figcaption><span>Figure 1: Test 1</span></figcaption>
+            <figure>
+              <img
+                src="https://solid-command-palette.vercel.app/images/command-palette-examples/tailwind.png"
+                alt="demo1"
+              />
+            </figure>
+            <figcaption><span>Figure 1: Test 1</span></figcaption>
+          </div>
+        </aside>
+      </section>
+    </main>
+    <footer>
+      Make By&nbsp;<span class="highlight">Team F4</span>&nbsp;and<span
+        class="highlight"
+        >Holistics</span
+      >
+    </footer>
   </div>
 </template>
 <style scoped>
-.main-section{
+.main-section {
   display: flex;
   align-items: center;
   flex: 1;
   gap: 10px;
 }
-.sologan{
+.sologan {
   font-size: 3rem;
   font-weight: 300;
   color: #42b883;
   line-height: 1.5;
   margin: 0;
-  padding:0;
+  padding: 0;
   border: none;
 }
-img{
+img {
   width: 100%;
   height: auto;
   border-radius: 12px;
   box-shadow: 0 1px 2px #0000000d;
 }
-aside{
+aside {
   flex: 1;
   padding-top: 50px;
   display: block;
 }
-.gwraper{
+.gwraper {
   display: flex;
-    flex-direction: column;
-    overflow-y: scroll;
-    height: 50vh;
-    padding: 0 10px;
-    scroll-snap-type: block mandatory;
-    overscroll-behavior: contain;
+  flex-direction: column;
+  overflow-y: scroll;
+  height: 50vh;
+  padding: 0 10px;
+  scroll-snap-type: block mandatory;
+  overscroll-behavior: contain;
 }
-figure{
+figure {
   display: flex;
-    flex-direction: column;
-    gap: 20px;
-    align-items: center;
-    scroll-snap-align: start;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  scroll-snap-align: start;
 }
-figcaption{
+figcaption {
   text-align: center;
 }
 </style>
